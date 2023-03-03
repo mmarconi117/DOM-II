9d290d4230f1d2c0ddb9ba6259bf8f71d7f45eeb
Michael Marconi <michaelziga2@gmail.com>
2023-03-02 20:04:57 -0600

    DOM2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// click
+document.body.addEventListener('click', evt =>{
+    evt.target.classList.toggle('mirror')
+})
+
+//dblClick
+
+document.body.addEventListener('dblclick', event =>{
+    event.target.outerHTML = 'GOTCHA!'
+})
+
+
+//keydown
+
+window.addEventListener("keydown", event =>{
+    if (event.key == 6){
+        document.body.innerHTML = '<h1>GOTCHA!!</h1>'
+    }
+})
+
+
+// wheel;\
+
+document.body.addEventListener('wheel', event =>{
+    event.target.classList.toggle('wheel')
+})
+
+
+//mouseenter/mouseleave
+
+const destinations = document.querySelectorAll('.destination')
+
+for (let destination of destinations){
+    destination.addEventListener('mouseenter', () =>{
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => [
+        destination.style.fontWeight = 'initial'
+    ])
+}
